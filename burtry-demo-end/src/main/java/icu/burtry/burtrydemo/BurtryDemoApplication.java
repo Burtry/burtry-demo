e8319abe6ddbce4b5899b2c1c9051b66ffbdf246
Burtry <1971797331@qq.com>
2024-10-22 17:42:57 +0800

    Init Code End

---
+++ burtry-demo-end/src/main/java/icu/burtry/burtrydemo/BurtryDemoApplication.java
@@ -0,0 +1,13 @@
+package icu.burtry.burtrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurtryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurtryDemoApplication.class, args);
+    }
+
+}
---
+++ burtry-demo-end/src/test/java/icu/burtry/burtrydemo/BurtryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.burtry.burtrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurtryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
