27c44d8c986594052043132bd6488612ceb7b9ac
Burtry <1971797331@qq.com>
2024-10-29 11:45:19 +0800

    修改需求文档 + pom文件依赖更新

--- burtry-demo-vue3/src/main.js
+++ burtry-demo-vue3/src/main.js
@@ -1,13 +1,17 @@
-import './assets/main.css'
-
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+//暗黑模式css
+// import 'element-plus/theme-chalk/dark/css-vars.css'
+
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
--- burtry-demo/writespace-service/writespace-user/src/main/java/icu/burtry/writespaceuser/WritespaceUserApplication.java
+++ burtry-demo/writespace-service/writespace-user/src/main/java/icu/burtry/writespaceuser/WritespaceUserApplication.java
@@ -2,8 +2,10 @@ package icu.burtry.writespaceuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WritespaceUserApplication {
 
     public static void main(String[] args) {
