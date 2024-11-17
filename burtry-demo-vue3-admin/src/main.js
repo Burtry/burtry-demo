7e1fe0b235324d22094a99ca41661bc63343078e
Burtry <1971797331@qq.com>
2024-11-17 21:36:49 +0800

    Update Code - 管理员登录逻辑实现

--- burtry-demo-vue3-admin/src/main.js
+++ burtry-demo-vue3-admin/src/main.js
@@ -1,9 +1,11 @@
 import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import './styles/common.scss'
+const pinia = createPinia()
 //暗黑模式css
 // import 'element-plus/theme-chalk/dark/css-vars.css'
 
@@ -14,9 +16,11 @@ import router from './router'
 const app = createApp(App)
 
 app.use(ElementPlus, {
-    locale: zhCn,
+  locale: zhCn,
 })
-app.use(createPinia())
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- burtry-demo-vue3-admin/src/utils/http.js
+++ burtry-demo-vue3-admin/src/utils/http.js
@@ -1,10 +1,68 @@
 import axios from 'axios';
 import 'element-plus/theme-chalk/el-message.css';
 
+import { ElMessage } from 'element-plus';
+import { useUserStore } from '../stores/user';
+import router from '@/router/index';
+
+
+
 const baseURL = '/api';
 const instance = axios.create({
   baseURL: baseURL,
   timeout: 5000
 });
 
-export default instance;
+// axios请求拦截器
+instance.interceptors.request.use(config => {
+  const userStore = useUserStore()
+
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.token = token
+  }
+  return config
+}, e => Promise.reject(e))
+
+
+//响应拦截器
+instance.interceptors.response.use(res => {
+  // 检查请求 URL 是否是验证码请求，如果是，则直接返回原始响应，不进行二次处理
+  if (res.config.url.includes('api/v1/code')) {
+    return res;  // 直接返回原始响应
+  }
+  return res.data
+}, e => {
+  const userStore = useUserStore()
+  // ElMessage({ message: e.response.data.msg, type: 'error' })
+  //401处理
+  if (e.response.status === 401) {
+    userStore.removeUserInfo()
+    ElMessage({ type: 'error', message: '登录过期，请重新登录' });
+    router.push('/login')
+  }
+  return Promise.reject(e)
+})
+
+// //响应拦截器
+// instance.interceptors.response.use(res => {
+//   // 检查请求 URL 是否是验证码请求，如果是，则直接返回原始响应，不进行二次处理
+//   if (res.config.url.includes('api/v1/code')) {
+//     return res;  // 直接返回原始响应
+//   }
+//   return res.data
+// }, e => {
+//   const userStore = useUserStore()
+//   const router = useRouter()
+//   //401处理
+//   if (e.response.status === 401) {
+//     console.log(e);
+
+
+//     userStore.removeUserInfo()
+//     ElMessage({ type: 'error', message: '登录过期，请重新登录' });
+//     router.push('/login')
+//   }
+//   return Promise.reject(e)
+// })
+export default instance
