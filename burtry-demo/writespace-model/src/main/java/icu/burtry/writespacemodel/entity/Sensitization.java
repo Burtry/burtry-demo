c60be81e18f1dc419868642d5d6209cb0651e3e3
Burtry <1971797331@qq.com>
2024-12-02 13:15:33 +0800

    Update Code --- 敏感词id生成策略修改

--- burtry-demo/writespace-model/src/main/java/icu/burtry/writespacemodel/entity/Sensitization.java
+++ burtry-demo/writespace-model/src/main/java/icu/burtry/writespacemodel/entity/Sensitization.java
@@ -1,5 +1,7 @@
 package icu.burtry.writespacemodel.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,6 +9,7 @@ import java.time.LocalDateTime;
 @Data
 public class Sensitization {
 
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private String sensitiveName;
