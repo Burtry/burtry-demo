378d1044af17a324899e7f5988a9c8ca89eba262
Burtry <1971797331@qq.com>
2024-12-11 20:17:27 +0800

    Update Code --- 发布文章 初接口

--- burtry-demo/writespace-feign-api/src/main/java/icu/burtry/apis/user/IUserClient.java
+++ burtry-demo/writespace-feign-api/src/main/java/icu/burtry/apis/user/IUserClient.java
@@ -1,5 +1,6 @@
 package icu.burtry.apis.user;
 
+import icu.burtry.writespacemodel.entity.Channel;
 import icu.burtry.writespacemodel.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,8 @@ public interface IUserClient {
     @GetMapping("/api/v1/user/{id}")
     User findUserById(@PathVariable("id") Long id);
 
+    @GetMapping("/api/v1/channel/{id}")
+    Channel getChannelById(@PathVariable Long id);
 
 
 }
